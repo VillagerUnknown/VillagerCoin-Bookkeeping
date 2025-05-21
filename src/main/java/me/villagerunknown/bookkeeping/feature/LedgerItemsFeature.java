f0cbba1c43b82f016ce4d3e419e07230d7424f10
villagerunknown <VillagerUnknown@proton.me>
2025-05-20 22:37:26 -0400

    Updates from VC

--- src/main/java/me/villagerunknown/bookkeeping/feature/LedgerItemsFeature.java
+++ src/main/java/me/villagerunknown/bookkeeping/feature/LedgerItemsFeature.java
@@ -2,6 +2,7 @@ package me.villagerunknown.bookkeeping.feature;
 
 import me.villagerunknown.bookkeeping.item.LedgerItem;
 import me.villagerunknown.bookkeeping.item.LedgerItems;
+import me.villagerunknown.villagercoin.Villagercoin;
 import me.villagerunknown.villagercoin.feature.LedgerFeature;
 import net.minecraft.item.Item;
 
@@ -12,11 +13,11 @@ public class LedgerItemsFeature {
 	}
 	
 	public static Item registerLedgerItem( String id, Item.Settings settings ) {
-		return LedgerFeature.registerLedger( id, new LedgerItem( settings ) );
+		return LedgerFeature.registerLedger( Villagercoin.MOD_ID, id, new LedgerItem( settings ) );
 	}
 	
 	public static Item registerCraftableLedgerItem( String id, Item.Settings settings ) {
-		return LedgerFeature.registerCraftableLedger( id, new LedgerItem( settings ) );
+		return LedgerFeature.registerCraftableLedger( Villagercoin.MOD_ID, id, new LedgerItem( settings ) );
 	}
 	
 }
--- src/main/java/me/villagerunknown/bookkeeping/feature/ReceiptItemsFeature.java
+++ src/main/java/me/villagerunknown/bookkeeping/feature/ReceiptItemsFeature.java
@@ -2,6 +2,7 @@ package me.villagerunknown.bookkeeping.feature;
 
 import me.villagerunknown.bookkeeping.item.ReceiptItem;
 import me.villagerunknown.bookkeeping.item.ReceiptItems;
+import me.villagerunknown.villagercoin.Villagercoin;
 import me.villagerunknown.villagercoin.feature.ReceiptFeature;
 import net.minecraft.item.Item;
 
@@ -12,11 +13,11 @@ public class ReceiptItemsFeature {
 	}
 	
 	public static Item registerReceiptItem( String id, Item.Settings settings ) {
-		return ReceiptFeature.registerReceipt( id, new ReceiptItem( settings ) );
+		return ReceiptFeature.registerReceipt( Villagercoin.MOD_ID, id, new ReceiptItem( settings ) );
 	}
 	
 	public static Item registerCraftableReceiptItem( String id, Item.Settings settings ) {
-		return ReceiptFeature.registerCraftableReceipt( id, new ReceiptItem( settings ) );
+		return ReceiptFeature.registerCraftableReceipt( Villagercoin.MOD_ID, id, new ReceiptItem( settings ) );
 	}
 	
 }
